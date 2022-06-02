1362ecff7f4819fb7f71307f867617dd1510ab7c
Ilya <enikeevdev@gmail.com>
2022-06-02 20:07:01 +0600

    #1 Initial commit

---
+++ src/test/java/com/a2/a2_automation_system/A2AutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2.a2_automation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2AutomationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
