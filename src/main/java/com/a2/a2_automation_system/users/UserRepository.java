64c909324124956aa117cd39c0afa785ca8f040d
BashirovAlfit <alfit.bashirov@gmail.com>
2022-06-08 13:33:05 +0600

    Added methods to user repository

--- src/main/java/com/a2/a2_automation_system/users/UserRepository.java
+++ src/main/java/com/a2/a2_automation_system/users/UserRepository.java
@@ -1,10 +1,17 @@
 package com.a2.a2_automation_system.users;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User,Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByLogin(String email);
 
+    Page<User> findAll(Pageable pageable);
+
+    Page<User> findAllByRole(Pageable pageable);
+
+    Page<User> findAllByIsActive(Pageable pageable);
 }
