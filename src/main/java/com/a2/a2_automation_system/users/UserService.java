eb85d06d529eee6c66621afb71146e5a54cba53a
TilekbakovaJamila <tilekbekovaja@gmail.com>
2022-06-08 17:52:24 +0900

    Implemented the getUsers method in UserService

--- src/main/java/com/a2/a2_automation_system/users/UserService.java
+++ src/main/java/com/a2/a2_automation_system/users/UserService.java
@@ -1,10 +1,15 @@
 package com.a2.a2_automation_system.users;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,4 +20,22 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
+
+
+    public Page<UserDTO> listUser(Pageable pageable, String role, boolean isActive) {
+        if (isActive) {
+            userRepository.findAllByIsActive(pageable, isActive);
+        } else if (isActive) {
+            userRepository.findAllByIsActive(pageable, isActive);
+        }else if(!role.equals(null)) {
+            userRepository.findAllByRole(pageable,role);
+        } else if (isActive && !role.equals(null)) {
+            userRepository.findAllByIsActiveAndRole(pageable,isActive,role);
+        }else if (!isActive &&  !role.equals(null)) {
+            userRepository.findAllByIsActiveAndRole(pageable,isActive,role);
+        }
+        return userRepository.findAll(pageable);
+    }
+
+
 }
