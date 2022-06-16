f14b895a87dd1ee857b1d7c8d3c39149a18cd22a
BashirovAlfit <alfit.bashirov@gmail.com>
2022-06-16 14:30:32 +0600

    #108 Update user service and added check login

---
+++ src/main/java/com/a2/a2_automation_system/exception/UserAlreadyRegisteredException.java
@@ -0,0 +1,10 @@
+package com.a2.a2_automation_system.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class UserAlreadyRegisteredException extends RuntimeException {
+
+
+}
