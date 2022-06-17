990b381c2ce98f86a8ebbb3c4025d2c85f85b9be
Ilya <enikeevdev@gmail.com>
2022-06-17 17:56:53 +0600

    #104 Added template and files for index page

--- src/main/java/com/a2/a2_automation_system/config/WebConfig.java
+++ src/main/java/com/a2/a2_automation_system/config/WebConfig.java
@@ -12,10 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:static/scripts/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:static/css/");
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations(String.format("file:%s/", fileStoreConfiguration.getPath()));
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+//        registry.addResourceHandler("/images/**")
+//                .addResourceLocations(String.format("file:%s/", fileStoreConfiguration.getPath()));
     }
 
 }
