bd6d373de56bc7b82e7ec6c42dbc0e9a28ab9ece
TilekbakovaJamila <tilekbekovaja@gmail.com>
2022-06-22 20:34:13 +0900

    #151 Created the MoneyMovement entity

---
+++ src/main/java/com/a2/a2_automation_system/money/MoneyMovement.java
@@ -0,0 +1,42 @@
+package com.a2.a2_automation_system.money;
+
+import com.a2.a2_automation_system.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "moneys")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MoneyMovement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Enumerated(value = EnumType.STRING)
+    @NotNull
+    private TypeOfFinance typeOfFinance;
+
+    @ManyToOne
+    @JoinColumn(name = "counterparty_id")
+    private User counterparty;
+
+    @NotNull
+    private Double amount;
+
+
+    private String purpose;
+
+
+    @NotNull
+    private LocalDate date;
+}
