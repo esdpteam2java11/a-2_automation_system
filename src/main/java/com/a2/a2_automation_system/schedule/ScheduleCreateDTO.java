eb6fc83b3785b14815b4c54861ecfadbb25a0865
kasdan <kasdan23@gmail.com>
2022-07-07 22:38:55 +0600

    #182 Added validation

--- src/main/java/com/a2/a2_automation_system/schedule/ScheduleCreateDTO.java
+++ src/main/java/com/a2/a2_automation_system/schedule/ScheduleCreateDTO.java
@@ -1,16 +1,14 @@
 package com.a2.a2_automation_system.schedule;
 
-import com.a2.a2_automation_system.exception.DateEnteredNotCorrect;
 import com.a2.a2_automation_system.group.Group;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
-import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -19,7 +17,6 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ScheduleCreateDTO {
-
     private Long id;
 
     @NotNull
@@ -34,6 +31,7 @@ public class ScheduleCreateDTO {
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime timeEnd;
 
+    @Future
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateEnd;
 
@@ -44,12 +42,5 @@ public class ScheduleCreateDTO {
     @NotNull
     private Group group;
 
-    public void eventStartDate(LocalDate date) {
-        if(date.isAfter(LocalDate.now())) {
-            eventStartDate = date;
-        } else {
-            throw new DateEnteredNotCorrect("Введите правильную дату");
-        }
-    }
 
 }
