faad86b96c1d838bd41165c439a83b3061ffad63
mamutova <nargiz.mamutova@gmail.com>
2022-07-12 03:48:05 +0600

    #224 refactor UserShortInfoDTO

---
+++ src/main/java/com/a2/a2_automation_system/user/UserShortInfoDTO.java
@@ -0,0 +1,32 @@
+package com.a2.a2_automation_system.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserShortInfoDTO {
+    private Long id;
+    private String fio;
+    private Date birthDate;
+    private double amount;
+    private String phone;
+
+    public static UserShortInfoDTO from(User user) {
+        return builder()
+                .id(user.getId())
+                .fio(user.getSurname() + " " + user.getName() + (user.getPatronymic() != null ?
+                        (" " + user.getPatronymic()) : ""))
+                .birthDate(user.getBirthDate())
+                .phone(user.getPhone())
+                .build();
+    }
+
+
+}
