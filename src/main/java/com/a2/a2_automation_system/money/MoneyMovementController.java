6c44e75f61fb50cf5c4155842afc82de229ff002
mamutova <nargiz.mamutova@gmail.com>
2022-07-12 03:55:56 +0600

    #224 put users into model

---
+++ src/main/java/com/a2/a2_automation_system/money/MoneyMovementController.java
@@ -0,0 +1,27 @@
+package com.a2.a2_automation_system.money;
+
+import com.a2.a2_automation_system.user.UserRepository;
+import com.a2.a2_automation_system.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/cash")
+public class MoneyMovementController {
+    private final UserService userService;
+
+    @PreAuthorize("hasAnyAuthority('ADMIN','EMPLOYEE')")
+    @GetMapping
+    public String getMoneyMovements(Model model) {
+        model.addAttribute("typesOfFinance", TypeOfFinance.getRusValues());
+        model.addAttribute("operationTypes", OperationType.getRusValues());
+        model.addAttribute("users", userService.getAllUsers());
+        return "cash_flow";
+    }
+}
+
