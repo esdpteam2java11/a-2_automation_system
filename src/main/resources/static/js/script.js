8e7aac71e80927ec1ff9f71e82e1ad4beb1e8498
mamutova <nargiz.mamutova@gmail.com>
2022-07-22 23:56:16 +0600

    #244 refactor templates and script

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -56,15 +56,15 @@ function addSelectedParent(parent) {
     }
     if (!isExists) {
         let newTrTag = document.createElement("tr")
-        newTrTag.innerHTML = `<td hidden><input type="hidden" name="p_id" value="${parent.id}">${parent.id}</input></td>
-                                  <td><input type="hidden" name="p_kinship" value="${parent.kinshipName}">${parent.kinship}</input></td>
-                                  <td><input type="hidden" name="p_surname" value="${parent.surname}">${parent.surname}</input></td>
-                                  <td><input type="hidden" name="p_name" value="${parent.name}">${parent.name}</input></td>
-                                  <td><input type="hidden" name="p_patronymic" value="${parent.patronymic != null ? parent.patronymic : ""}">${parent.patronymic != null ? parent.patronymic : ""}</input></td>
-                                  <td><input type="hidden" name="p_phone" value="${parent.phone}">${parent.phone}</input></td>
-                                  <td><input type="hidden" name="p_whatsapp" value="${parent.whatsapp != null ? parent.whatsapp : ""}">${parent.whatsapp != null ? parent.whatsapp : ""}</input></td>
-                                  <td><input type="hidden" name="p_telegram" value="${parent.telegram != null ? parent.telegram : ""}">${parent.telegram != null ? parent.telegram : ""}</input></td>
-                                  <td>
+        newTrTag.innerHTML = `<td class="p-0" hidden><input type="hidden" name="p_id" value="${parent.id}">${parent.id}</input></td>
+                                  <td class="p-0 m-0"><input class="form-control" type="hidden" name="p_kinship" value="${parent.kinshipName}">${parent.kinship}</input></td>
+                                  <td class="p-0 m-0"><input class="form-control" type="hidden" name="p_surname" value="${parent.surname}">${parent.surname}</input></td>
+                                  <td class="p-0 m-0"><input class="form-control" type="hidden" name="p_name" value="${parent.name}">${parent.name}</input></td>
+                                  <td class="p-0 m-0"><input class="form-control" type="hidden" name="p_patronymic" value="${parent.patronymic != null ? parent.patronymic : ""}">${parent.patronymic != null ? parent.patronymic : ""}</input></td>
+                                  <td class="p-0 m-0"><input class="form-control" type="hidden" name="p_phone" value="${parent.phone}">${parent.phone}</input></td>
+                                  <td class="p-0 m-0"<input class="form-control" type="hidden" name="p_whatsapp" value="${parent.whatsapp != null ? parent.whatsapp : ""}">${parent.whatsapp != null ? parent.whatsapp : ""}</input></td>
+                                  <td class="p-0 m-0"><input class="form-control" type="hidden" name="p_telegram" value="${parent.telegram != null ? parent.telegram : ""}">${parent.telegram != null ? parent.telegram : ""}</input></td>
+                                  <td class="text-center p-0 m-0">
                                         <button type="button" class="btn btn-outline-danger py-1 px-2" onclick='deleteSelectedRow(this)'>
                                             <i class="bi bi-x-square"></i>
                                         </button>
@@ -82,8 +82,8 @@ const addNewParentBtn = document.getElementById("add-new-parent-button")
 addNewParentBtn.addEventListener("click", function () {
     const newTrTag = document.createElement("tr")
     newTrTag.innerHTML = `<td class="p-0" hidden><input type="hidden" name="p_id" value="0"/></td>
-                          <td class="p-0">
-                                <select name="p_kinship" required>
+                          <td class="p-0 m-0">
+                                <select class="form-control" name="p_kinship" required>
                                     <option value="FATHER">Отец</option>
                                     <option value="MOTHER">Мать</option>
                                     <option value="GRANDMOTHER">Бабушка</option>
@@ -93,13 +93,13 @@ addNewParentBtn.addEventListener("click", function () {
                                     <option value="GUARDIAN">Опекун</option>
                                 </select>
                           </td>
-                          <td><input type="text" name="p_surname" required/></td>
-                          <td><input type="text" name="p_name" required/></td>
-                          <td><input type="text" name="p_patronymic" value=" "/></td>
-                          <td><input type="text" name="p_phone" required/></td>
-                          <td><input type="text" name="p_whatsapp" value=" "/></td>
-                          <td><input type="text" name="p_telegram" value=" "/></td>
-                          <td>
+                          <td class="p-0 m-0"><input class="form-control" type="text" name="p_surname" required/></td>
+                          <td class="p-0 m-0"><input class="form-control" type="text" name="p_patronymic" value=" "/></td>
+                          <td class="p-0 m-0"><input class="form-control" type="text" name="p_name" required/></td>
+                          <td class="p-0 m-0"><input class="form-control" type="text" name="p_phone" required/></td>
+                          <td class="p-0 m-0"><input class="form-control" type="text" name="p_whatsapp" value=" "/></td>
+                          <td class="p-0 m-0"><input class="form-control" type="text" name="p_telegram" value=" "/></td>
+                          <td class="text-center p-0 m-0">
                                 <button type="button" class="btn btn-outline-danger py-1 px-2" onclick='deleteSelectedRow(this)'>
                                      <i class="bi bi-x-square"></i>
                                 </button>
@@ -123,13 +123,13 @@ searchRelativeForm.addEventListener("submit", async function (e) {
         for (let i = 0; i < data.length; i++) {
             var parent = JSON.stringify(data[i]);
             let newTrTag = document.createElement("tr")
-            newTrTag.innerHTML = `<td hidden>${data[i].id}</td>
-                                  <td>${data[i].kinship}</td>
-                                  <td>${data[i].surname}</td>
-                                  <td>${data[i].name}</td>
-                                  <td>${data[i].patronymic != null ? data[i].patronymic : ""}</td>
-                                  <td>
-                                        <input class="form-check-input" type="checkbox" onclick='addSelectedParent(${parent})'>
+            newTrTag.innerHTML = `<td class="text-center" hidden>${data[i].id}</td>
+                                  <td class="text-center">${data[i].kinship}</td>
+                                  <td class="text-center">${data[i].surname}</td>
+                                  <td class="text-center">${data[i].name}</td>
+                                  <td class="text-center">${data[i].patronymic != null ? data[i].patronymic : ""}</td>
+                                  <td class="text-center">
+                                        <input type="checkbox" onclick='addSelectedParent(${parent})'>
                                         <label class="form-check-label" for="inlineCheckbox"></label>
                                   </td>`
             tbody.appendChild(newTrTag)
