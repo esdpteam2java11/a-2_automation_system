acca80d1f3b59818ba539ffe380573d75a05de75
kasdan <kasdan23@gmail.com>
2022-07-26 21:08:34 +0600

    #271 updated controllers, templates and methods

--- src/main/java/com/a2/a2_automation_system/user/UserController.java
+++ src/main/java/com/a2/a2_automation_system/user/UserController.java
@@ -258,6 +258,8 @@ public class UserController {
         }
         return authentication.isAuthenticated();
     }
+    @GetMapping("/calendar_sportsman/all/")
+    public String getAllCalendarForSportsman(Model model){return "calendar_all_events_sportsman_view";}
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(FORBIDDEN)
