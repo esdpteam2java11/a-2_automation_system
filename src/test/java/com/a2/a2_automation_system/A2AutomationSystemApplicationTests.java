f05e92abd107c7d948f2e0bc58aec85460ceffe1
Daniar Kasygulov <61041855+kasdan@users.noreply.github.com>
2022-08-11 23:10:16 +0600

    Update A2AutomationSystemApplicationTests.java

--- src/test/java/com/a2/a2_automation_system/A2AutomationSystemApplicationTests.java
+++ src/test/java/com/a2/a2_automation_system/A2AutomationSystemApplicationTests.java
@@ -5,6 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class A2AutomationSystemApplicationTests {
+  
+@Test
+     void contextLoads() {
+    }
+
 
 }
