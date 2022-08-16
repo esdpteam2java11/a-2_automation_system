89c4597c03581d77176e753dc012c8bf6134be01
mamutova <nargiz.mamutova@gmail.com>
2022-08-17 04:38:05 +0600

    #246 implemented controller method

--- src/main/java/com/a2/a2_automation_system/sportsmanpayments/SportsmanPaymentController.java
+++ src/main/java/com/a2/a2_automation_system/sportsmanpayments/SportsmanPaymentController.java
@@ -1,26 +1,29 @@
 package com.a2.a2_automation_system.sportsmanpayments;
 
-import com.a2.a2_automation_system.user.UserDTO;
-import com.a2.a2_automation_system.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import java.time.YearMonth;
 
 @Controller
 @RequiredArgsConstructor
 public class SportsmanPaymentController {
     private final SportsmanPaymentService sportsmanPaymentService;
-    private final UserService userService;
 
     @PreAuthorize("hasAnyAuthority('ADMIN','EMPLOYEE')")
     @GetMapping("/all_debts")
-    public String getAllDebtsReport(Model model, HttpServletRequest request){
-        UserDTO user = UserDTO.from(userService.getUserByUsername(request.getRemoteUser()));
-        model.addAttribute("sportsman",user);
+    public String getAllDebtsReport(Model model,
+                                    @RequestParam @Nullable YearMonth startDate,
+                                    @RequestParam @Nullable YearMonth endDate){
+        if (startDate!=null && endDate!=null) {
+            model.addAttribute("sportsmenPayments",sportsmanPaymentService
+                    .getSportsmanPayments(startDate, endDate));
+        }
         return "all_debts_report";
     }
 }
