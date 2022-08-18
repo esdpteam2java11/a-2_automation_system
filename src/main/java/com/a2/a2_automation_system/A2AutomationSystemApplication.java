26239d413e50927dc66d1efe7f3ab63de19038ba
kasdan <kasdan23@gmail.com>
2022-08-18 19:05:13 +0600

    #320 added logic to check attendance

--- src/main/java/com/a2/a2_automation_system/A2AutomationSystemApplication.java
+++ src/main/java/com/a2/a2_automation_system/A2AutomationSystemApplication.java
@@ -3,12 +3,12 @@ package com.a2.a2_automation_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class A2AutomationSystemApplication {
+    @SpringBootApplication
+    public class A2AutomationSystemApplication {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        SpringApplication.run(A2AutomationSystemApplication.class, args);
-    }
+            SpringApplication.run(A2AutomationSystemApplication.class, args);
+        }
 
 }
