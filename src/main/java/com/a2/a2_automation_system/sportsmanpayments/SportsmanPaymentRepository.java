092424a5c0c092dc1aa0a72edb21f33e68c64451
mamutova <nargiz.mamutova@gmail.com>
2022-08-19 01:06:07 +0600

    #246 fixed queries to db

--- src/main/java/com/a2/a2_automation_system/sportsmanpayments/SportsmanPaymentRepository.java
+++ src/main/java/com/a2/a2_automation_system/sportsmanpayments/SportsmanPaymentRepository.java
@@ -15,6 +15,14 @@ public interface SportsmanPaymentRepository extends PagingAndSortingRepository<S
             "and sp.operation_type = ?2 order by sp.date desc limit 1", nativeQuery = true)
     Optional<SportsmanPayment> findUpToDateAmount(Long user, String type);
 
+    @Query(value = "select * from sportsman_payments sp " +
+            "where sp.id in (select max(id) from sportsman_payments " +
+            "where operation_type = 'ACCRUED'and user_id = ?1 " +
+            "group by date_part('year', date), date_part('month', date)) " +
+            "and sp.date < ?2 " +
+            "order by sp.date", nativeQuery = true)
+    List<SportsmanPayment> findAccruedAmountsBeforeDate(Long user, Date date);
+
     @Query(value = "select * from sportsman_payments sp where sp.user_id = ?1 " +
             "and sp.operation_type = 'ACCRUED' and date_part('year',sp.date) <= ?2 " +
             "and date_part('month',sp.date) <= ?3 order by sp.date desc limit 1", nativeQuery = true)
@@ -25,9 +33,13 @@ public interface SportsmanPaymentRepository extends PagingAndSortingRepository<S
             "and date_part('month',sp.date) = ?3 order by sp.date desc limit 1", nativeQuery = true)
     Optional<SportsmanPayment> findAccruedAmountForPeriod(Long user, Integer year, Integer month);
 
-    @Query(value = "select sum(sp.amount) from SportsmanPayment sp where sp.user.id = ?1 " +
+    @Query(value = "select coalesce(sum(sp.amount), 0) from SportsmanPayment sp where sp.user.id = ?1 " +
             "and sp.operationType = 'PAID' and sp.date >= ?2 and sp.date <= ?3")
     Double findTotalPaidAmountForPeriod(Long user, Date startDate, Date endDate);
+
+    @Query(value = "select coalesce(sum(sp.amount), 0) from SportsmanPayment sp where sp.user.id = ?1 " +
+            "and sp.operationType = 'PAID' and sp.date < ?2")
+    Double findTotalPaidAmountToDate(Long user, Date date);
 
     List<SportsmanPayment> findByUserId(Long id);
 }
