d38c8fe79b0d31377889621f59420674312db1d5
mamutova <nargiz.mamutova@gmail.com>
2022-08-25 15:52:58 +0600

    #375 deleted sportsmen initiation

--- src/main/java/com/a2/a2_automation_system/util/InitDatabase.java
+++ src/main/java/com/a2/a2_automation_system/util/InitDatabase.java
@@ -1,22 +1,15 @@
 package com.a2.a2_automation_system.util;
 
-import com.a2.a2_automation_system.sportsmanpayments.OperationType;
-import com.a2.a2_automation_system.sportsmanpayments.SportsmanPayment;
-import com.a2.a2_automation_system.sportsmanpayments.SportsmanPaymentRepository;
 import com.a2.a2_automation_system.user.Role;
 import com.a2.a2_automation_system.group.Group;
 import com.a2.a2_automation_system.group.GroupRepository;
 import com.a2.a2_automation_system.user.User;
 import com.a2.a2_automation_system.user.UserRepository;
-import com.a2.a2_automation_system.userparam.UserParam;
-import com.a2.a2_automation_system.userparam.UserParamRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 @Configuration
@@ -28,9 +21,7 @@ public class InitDatabase {
     }
 
     @Bean
-    CommandLineRunner init(UserRepository userRepository, GroupRepository groupRepository,
-                           SportsmanPaymentRepository sportsmanPaymentRepository,
-                           UserParamRepository userParamRepository) {
+    CommandLineRunner init(UserRepository userRepository, GroupRepository groupRepository) {
         return (args) -> {
             if (userRepository.findByLogin("admin").isEmpty()) {
                 User admin = new User();
@@ -75,38 +66,6 @@ public class InitDatabase {
                                 .trainer(userRepository.findByLogin("arsen")
                                         .orElse(null)).color("#34e059").build());
                 groupRepository.saveAll(groups);
-            }
-            for (int i = 0; i < 19; i++) {
-                if (userRepository.findByLogin("student_" + i).isEmpty()) {
-                    User student = new User();
-                    student.setSurname("Фамилия_" + i);
-                    student.setName("Имя_" + i);
-                    student.setPatronymic("Отчество_" + i);
-                    student.setPhone("33-33-33");
-                    student.setAddress("г. Бишкек, ул. Ахунбаева 26");
-                    student.setBirthDate(new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2010"));
-                    student.setRole(Role.CLIENT);
-                    student.setLogin("student_" + i);
-                    student.setPassword(passwordEncoder.encode("123"));
-                    student.setIsActive(true);
-                    student.setGroup(groups.get((int) (Math.random() * groups.size())));
-                    student.setDateOfAdmission(new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2022"));
-                    userRepository.save(student);
-
-                    SportsmanPayment sportsmanPayment = new SportsmanPayment();
-                    sportsmanPayment.setAmount((double) student.getGroup().getSum());
-                    sportsmanPayment.setDate(student.getDateOfAdmission());
-                    sportsmanPayment.setUser(student);
-                    sportsmanPayment.setOperationType(OperationType.ACCRUED);
-                    sportsmanPaymentRepository.save(sportsmanPayment);
-
-                    userParamRepository.save(UserParam.builder()
-                            .height(165.0)
-                            .weight(65.0)
-                            .creationDate(new Date())
-                            .user(student)
-                            .build());
-                }
             }
         };
     }
